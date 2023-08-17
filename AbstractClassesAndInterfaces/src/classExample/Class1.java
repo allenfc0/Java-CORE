a1cb971f27ad2e0895f7279ebdc5b9c86c12cc18
Allen Farias <allenfarias29@gmail.com>
2023-08-16 23:21:13 -0700

    created java class

---
+++ AbstractClassesAndInterfaces/src/classExample/Class1.java
@@ -0,0 +1,17 @@
+package classExample;
+
+import abstractClassExample.Abstract1;
+import interfaceExample.Interface1;
+import interfaceExample.Interface2;
+
+public class Class1 implements Interface1, Interface2{
+
+	@Override
+	public void runningSpeed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
